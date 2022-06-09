3eb0bee3bcd5fa6f4205ef87e83053fb14793b8a
CougarChan <gohan0136@gmail.com>
2022-06-09 18:56:14 -0400

    Finished code editing and cleaning

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -1,20 +1,13 @@
-// declares a brand new database
+
 const request = window.indexedDB.open("budget-tracker", 1);
 let db;
 
-// initialize the database with onupgradeneeded
-// create a new object storage with a name
-// modify the object storage by opening up a new transaction and ".objectStore"
-// need  to open up a new transaction every time the object storage must be modified
-
 request.onupgradeneeded = (event) => {
-  db = event.target.result; // this result is an object that represents a connection to the database
-
+  db = event.target.result; 
   db.createObjectStore("Budget", { autoIncrement: true });
 };
 
 request.onsuccess = (event) => {
-  //when the db is successfully created, then ...
   db = event.target.result;
   if (navigator.onLine) {
     uploadBudgetInfo();
@@ -23,9 +16,13 @@ request.onsuccess = (event) => {
 
 function saveRecord(data) {
   console.log(data);
-  const transaction = db.transaction(["Budget"], "readwrite"); // open a new transaction in your database object store with read/write perms
-  const budgetStore = transaction.objectStore("Budget"); // access the newly created transaction (set to your object store)
-  budgetStore.add(data); // add the data
+
+   
+ const transaction = db.transaction(["Budget"], "readwrite");
+             
+             
+const budgetStore = transaction.objectStore("Budget");
+  budgetStore.add(data); 
 }
 
 async function uploadBudgetInfo() {
@@ -40,6 +37,8 @@ async function uploadBudgetInfo() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(budgetInfo.result),
       })
+
+
         .then((result) => result.json())
         .then((response) => {
           if (response.message) {
@@ -49,7 +48,7 @@ async function uploadBudgetInfo() {
           const budgetStore = transaction.objectStore("Budget");
           budgetStore.clear();
 
-          alert("Your budget info has been synced!");
+          alert("Your info have all been synchronized!");
           window.location.reload();
         });
     }
